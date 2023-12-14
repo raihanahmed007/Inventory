e23a8eeee02c4dbfcec84480d3db13a0844d5104
Raihan Ahmed <Raihan Ahmed@DESKTOP-N5CFHLI>
2023-12-14 06:04:32 +0600

    Add project files.

---
+++ Inventory.Models/Bill.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Bill
+    {
+        public int BillId { get; set; }
+        [Display(Name = "Bill / Invoice Number")]
+        public string BillName { get; set; }
+        [Display(Name = "GRN")]
+        public int GoodsReceiveNoteId { get; set; }
+        [Display(Name = "Vendor Delivery Order")]
+        public string VendorDoNumber { get; set; }
+        [Display(Name = "Vendor Bill/ Incoice")]
+        public string VendorInvoiceNumber { get; set; }
+        [Display(Name = "Bill Date")]
+        public DateTimeOffset BillDate { get; set; }
+        [Display(Name = "Bill Date")]
+        public DateTimeOffset BillDueDate { get; set; }
+        [Display(Name = "Bill Type")] 
+        public int BillTypeId { get; set;}
+    }
+}
---
+++ Inventory.Models/Item.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class Item
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public int AvailQuantity { get; set; }
+        public Stasus Satuss { get; set; }
+
+    }
+
+}
+
+namespace Inventory.Models
+{
+    public enum Stasus
+    {
+        Ok, Defective
+    }
+}
