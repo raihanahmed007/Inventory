09f08b297c99c9725337279a8ea8f81e485a77ab
Raihan Ahmed <Raihan Ahmed@DESKTOP-N5CFHLI>
2023-12-16 18:48:47 +0600

    day2.3

---
+++ Inventory.Models/PaymentType.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class PaymentType
+    {
+        public int PaymentTypeId { get; set; }
+        [Required]
+        [Display(Name = "Payment Type Name")]
+        public string PaymentTypeName { get; set; }
+        public string Descrioption { get; set; }
+    }
+}
---
+++ Inventory.Models/ShipmentType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class ShipmentType
+    {
+        public int Id { get; set; }
+        public string ShipmentTypeName { get; set; }
+        public string Desription { get; set; }
+    }
+}
