0074c5c4c1613bf5854ece601ad6cd3cbf7f4f91
Raihan Ahmed <Raihan Ahmed@DESKTOP-N5CFHLI>
2023-12-16 19:12:54 +0600

    day 2.4

---
+++ Inventory.Models/NumberSequence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Models
+{
+    public class NumberSequence
+    {
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Module { get; set; }
+        [Required]
+        public string Profile { get; set; }
+        [Required]
+        public int LastNumber { get; set; }
+
+
+    }
+}
