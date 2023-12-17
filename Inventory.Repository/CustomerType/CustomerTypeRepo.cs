d66286ea76378bda8a3406bf8aa0e52d2f968e82
Raihan Ahmed <Raihan Ahmed@DESKTOP-N5CFHLI>
2023-12-17 23:09:16 +0600

    day5

---
+++ Inventory.Repository/CustomerType/CustomerTypeRepo.cs
@@ -0,0 +1,24 @@
+﻿using Inventory.Repository.Paning;
+using Inventory.ViewModel.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.Repository.CustomerType
+{
+    public class CustomerTypeRepo : ICustomerTypeRepo
+    {
+        private ApplicationDbContext _context;
+        public CustomerTypeRepo(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public Task<PaginatedList<CustomerTypeListViewModel>> GetAll(int pageSize, int pageNumber)
+        {
+            var customerTypeList = _context.CustomersType;
+            var vm = customerTypeList.ModelToVm().AsQuerable();
+        }
+    }
+}
---
+++ Inventory.ViewModel/Maping/Relationship.cs
@@ -0,0 +1,30 @@
+﻿using Inventory.Models;
+using Inventory.ViewModel.Customer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory.ViewModel.Maping
+{
+    public static class Relationship
+    { 
+        public static IEnumerable<CustomerTypeListViewModel> ModelToVM(this IEnumerable<CustomerType> customerType)
+        {
+            List<CustomerTypeListViewModel> list = new List<CustomerTypeListViewModel>();
+            foreach (var ct in customerType)
+            {
+                list.Add(new CustomerTypeListViewModel()
+                {
+                    CustomerTypeId = ct.CustomerTypeId,
+                    CustomerTypeName = ct.CustomerTypeName,
+                    Description = ct.Description,
+                });
+                
+            }
+            return list;
+
+        }
+    }
+}
